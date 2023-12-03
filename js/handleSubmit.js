293132e05bf9902b75dff09a6c9f554ed4606dda
Krastage <tureev777@gmail.com>
2023-12-03 14:17:44 +0300

    fix adaptive

--- js/handleSubmit.js
+++
@@ -1,6 +0,0 @@
-const handleSubmit = (event) => {
-  event.preventDefault();
-};
-
-const button = document.querySelector('#submitButton');
-button.addEventListener('click', handleSubmit);
---
+++ js/index.js
@@ -0,0 +1,20 @@
+const study = document.querySelector('#study');
+const consult = document.querySelector('#consult');
+const submitButton = document.querySelector('#submitButton');
+
+// функция скроллинга к блоку с ценами
+const scrollToStudy = () => {
+  study.scrollIntoView({behavior: 'smooth'});
+};
+
+// функция скроллинга к блоку с формой
+const scrollToConsult = () => {
+  consult.scrollIntoView({behavior: 'smooth'});
+};
+
+// функция обработчик нажатия на кнопку отправить в форме
+const handleSubmit = (event) => {
+  event.preventDefault();
+};
+
+submitButton.addEventListener('click', handleSubmit);
